60bb53cb557870df4bb3962514cfee3908e2bc9c
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-04-26 15:30:05 +0700

    [*] Initramfs: Remove stmfb modules after firmware upgrade and before software reseting.

--- src/initramfs/overlay/etc/init.d/S15stmfb.sh
+++ src/initramfs/overlay/etc/init.d/S15stmfb.sh
@@ -2,11 +2,17 @@
 
 STMFB_PARAMS=display0=720x576-24@50i:3M:0:PAL:CVBS:CVBS
 
+insmodSafe() {
+	local module=$1
+	shift
+	test -e "$module" && insmod $module "$@"
+}
+
 case "$1" in
 	start)
 		echo -n "Starting stmfb... "
-		insmod /lib/modules/STLinux-2.4/stmcore-display-sti7105.ko
-		insmod /lib/modules/STLinux-2.4/stmfb.ko $STMFB_PARAMS
+		insmodSafe /lib/modules/STLinux-2.4/stmcore-display-sti7105.ko
+		insmodSafe /lib/modules/STLinux-2.4/stmfb.ko $STMFB_PARAMS
 
 		/opt/elecard/bin/fb_logo &
 		echo "done"
