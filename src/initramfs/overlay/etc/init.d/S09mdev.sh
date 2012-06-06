e7b3af1e5499da84ead8719405f5d6c15c2fc1ba
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-06-06 11:22:09 +0700

    [*] Initramfs+Rootfs: Remove unnecessary unmount/mount fs. Add mounting rootfs in background.

--- src/initramfs/overlay/etc/init.d/S09mdev.sh
+++ src/initramfs/overlay/etc/init.d/S09mdev.sh
@@ -6,16 +6,19 @@
 case "$1" in
 	start)
 		echo -n "Starting mdev... "
-		/bin/mount -t tmpfs mdev /dev
 		/bin/mkdir -p /dev/pts
+		mount -t devpts devpts /dev/pts
 		echo /sbin/mdev > /proc/sys/kernel/hotplug
-#is need scanning???
-		mdev -s
+#		if [ `lsusb | grep -c .` -gt 4 ]; then #first 4 devices are build in STi7105 SoC
+#		if [ `ls /sys/block/ | grep "sd." -c` -gt 0 ]; then #check block devices
+#			echo n "Scanning /sys ... "
+			mdev -s
+#		fi
 		echo "done"
 		;;
 	stop)
 		echo  > /proc/sys/kernel/hotplug
-		umount /dev
+		umount /dev/pts
 		;;
 	*)
 		echo "Usage: $0 {start|stop}"
