45e29c71ae70e37b50ac63d5312a884b1995eb29
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-06-29 11:26:16 +0700

    [*] Initramfs: Change parameter name from UPDATER_CHECK_ALWAYS to UPDATER_CHECK_NETWORK.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/initramfs/overlay/opt/elecard/bin/need_network.sh
+++ src/initramfs/overlay/opt/elecard/bin/need_network.sh
@@ -5,11 +5,11 @@ TEST_FIRMWARE=
 [% ELSE -%]
 TEST_FIRMWARE=1
 [% END -%]
-UPDATER_CHECK_ALWAYS=`/opt/elecard/bin/hwconfigManager a 0 UPDATER_CHECK_ALWAYS 2>/dev/null | grep "^VALUE:" | cut -d ' ' -f 2`
+UPDATER_CHECK_NETWORK=`/opt/elecard/bin/hwconfigManager a 0 UPDATER_CHECK_NETWORK 2>/dev/null | grep "^VALUE:" | cut -d ' ' -f 2`
 UPDATER_FOUND_UPDATE=`/opt/elecard/bin/hwconfigManager a 0 UPDATER_FOUND_UPDATE 2>/dev/null | grep "^VALUE:" | cut -d ' ' -f 2`
 
 NETWORK_NEED=
-if [ "$TEST_FIRMWARE" -o "${UPDATER_CHECK_ALWAYS:-0}" != "0" -o "${UPDATER_FOUND_UPDATE:-0}" != "0" ]; then
+if [ "$TEST_FIRMWARE" -o "${UPDATER_CHECK_NETWORK:-0}" != "0" -o "${UPDATER_FOUND_UPDATE:-0}" != "0" ]; then
 	NETWORK_NEED=1
 fi
 export NETWORK_NEED
