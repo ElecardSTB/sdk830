7d8c9323374ed50061d89b79a463c90ef1010d62
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-07-10 00:01:06 +0700

    [*] Initramfs: Return libdl library into initramfs.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/script/003_libnss_dns.sh
+++ src/script/003_libnss_dns.sh
@@ -2,5 +2,6 @@
 
 if [ -d $BUILDROOT/initramfs/lib -a -d $BUILDROOT/rootfs/lib ]; then
 	cp -dfp $BUILDROOT/rootfs/lib/libnss_dns* $BUILDROOT/initramfs/lib/
+	cp -dfp $BUILDROOT/rootfs/lib/libdl* $BUILDROOT/initramfs/lib/
 	rm -f $BUILDROOT/timestamps/.makebuildroot_initramfs
 fi
