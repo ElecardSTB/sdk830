24ae7ec8536337f30ce2a1b3e65d44bc83b62403
Petr.Yagunov <Petr.Yagunov@elecard.ru>
2012-07-10 17:55:42 +0700

    [+] Added teletes define, put script files in git

---
+++ src/update/stb830_development_teletes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+source $(dirname $0)/../../etc/checkEnvs.sh
+source $PRJROOT/src/update/default_env.sh
+
+UPD_CONFIG=dev
+
+BUILD_SCRIPT_FW=teletes
+#BUILD_WITHOUT_COMPONENTS_FW=1
+
+#Comment that add to firmware pack name
+SHORT_COMMENT=teletes_updateaddr
+
+ENABLE_TELETES=1
+
+make -C $PRJROOT firmware
+
