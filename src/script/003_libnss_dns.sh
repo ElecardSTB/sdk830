24831a2491219a00dfdc107f22a95ff9cc9dc420
Andrey.Kuleshov <Andrey.Kuleshov@elecard.ru>
2012-07-17 15:37:18 +0700

    [+] Build: Fix missing libresolv library in initramfs.

--- src/script/003_libnss_dns.sh
+++ src/script/003_libnss_dns.sh
@@ -3,5 +3,6 @@
 if [ -d $BUILDROOT/initramfs/lib -a -d $BUILDROOT/rootfs/lib ]; then
 	cp -dfp $BUILDROOT/rootfs/lib/libnss_dns* $BUILDROOT/initramfs/lib/
 	cp -dfp $BUILDROOT/rootfs/lib/libdl* $BUILDROOT/initramfs/lib/
+	cp -dfp $BUILDROOT/rootfs/lib/libresolv* $BUILDROOT/initramfs/lib/
 	rm -f $BUILDROOT/timestamps/.makebuildroot_initramfs
 fi
