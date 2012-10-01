ad8ea1da2cd510f4f5ffefa5827a09cb467aac24
Andrey.Kuleshov <Andrey.Kuleshov@elecard.ru>
2012-10-01 18:15:25 +0700

    [-] Rootfs: Fix static DNS option overrides DHCP

---
+++ src/rootfs/overlay/opt/elecard/bin/wifi_down.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Redirect stderr to /dev/null
+exec 3>&2
+exec 2>/dev/null
+
+wifi_led.sh 0
+
+kill `cat /var/run/udhcpc.wlan0.pid`
+killall hostapd
+killall wpa_supplicant
+rm -rf /var/run/wpa_supplicant
+
+# Restore stderr
+exec 2>&3 3>&-
+
+exit 0
