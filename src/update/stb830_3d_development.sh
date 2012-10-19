c2590fe6496bbb36e80429e9ccfa2098275dea3c
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-10-19 17:37:44 +0700

    [*] Build: Add cleaning StbMainApp in building 3d firmware scrip
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/update/stb830_3d_development.sh
+++ src/update/stb830_3d_development.sh
@@ -11,17 +11,20 @@ UPD_CONFIG=dev
 #Comment that add to firmware pack name
 SHORT_COMMENT=3D
 
+# Rebuild STAPI module and StbMainApp with ENABLE_3DRENDERING=1
 export ENABLE_3DRENDERING=1
-#rebuild STAPI module with difine ENABLE_3DRENDERING=1
+make -C $PRJROOT/src/apps/StbMainApp clean
 make -C $STSDKROOT/stapp purge_apilib MODULE=sthdmi
 #this is workaround for build sthdmi error
 [ -e $DVD_MAKE/Modules.symvers ] || echo -n " " > $DVD_MAKE/Modules.symvers
 make -C $STSDKROOT/stapp apilib MODULE=sthdmi
 
+# Build 3D firmware
 make -C $PRJROOT firmware
 
-#rebuild STAPI module to avoid building 3D firmares afterwards.
+# Rebuild STAPI module to avoid building 3D firmares afterwards
 export -n ENABLE_3DRENDERING
+make -C $PRJROOT/src/apps/StbMainApp clean
 make -C $STSDKROOT/stapp purge_apilib MODULE=sthdmi
 #this is workaround for build sthdmi error
 [ -e $DVD_MAKE/Modules.symvers ] || echo -n " " > $DVD_MAKE/Modules.symvers
