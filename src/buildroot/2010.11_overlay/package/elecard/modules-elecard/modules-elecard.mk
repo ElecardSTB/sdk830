5940e8884dbe8a4eb5940c14b22278a7256ab63a
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-11-25 03:33:54 +0700

    [*] Buildroot: Use GENTARGETS macros for modules-elecard in SDK build
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/modules-elecard/modules-elecard.mk
+++ src/buildroot/2010.11_overlay/package/elecard/modules-elecard/modules-elecard.mk
@@ -13,32 +13,28 @@ modules-elecard-install:
 modules-elecard-dirclean modules-elecard-clean:
 	make -C $(PRJROOT)/src/elecard/modules clean
 
-else #ifeq ($(STB830_SDK),)
-
-include package/elecard/sdk_env
-MODULES_ELECARD_PACKAGE:=modules-elecard-$(MODULES_ELECARD_PACK_VERSION).tar.gz
-MODULES_ELECARD_PACKAGE_DIR:=$(BUILD_DIR)/modules-elecard-$(MODULES_ELECARD_PACK_VERSION)
-INSTALL_DIR := $(TARGET_DIR)/lib/modules/STLinux-$(LINUX_VERSION)
-
-$(DL_DIR)/$(MODULES_ELECARD_PACKAGE):
-	 $(call DOWNLOAD,$(ELECARD_UPLOAD_SERVER),$(MODULES_ELECARD_PACKAGE))
-
-$(MODULES_ELECARD_PACKAGE_DIR)/.unpacked: $(DL_DIR)/$(MODULES_ELECARD_PACKAGE)
-	mkdir -p $(MODULES_ELECARD_PACKAGE_DIR)
-	$(ZCAT) $(DL_DIR)/$(MODULES_ELECARD_PACKAGE) | tar -C $(MODULES_ELECARD_PACKAGE_DIR) $(TAR_OPTIONS) -
-	touch $@
-
-modules-elecard-install: $(MODULES_ELECARD_PACKAGE_DIR)/.unpacked
-	mkdir -p $(INSTALL_DIR)
-	install -m 644 -p $(MODULES_ELECARD_PACKAGE_DIR)/st_dvb/st_dvb.ko $(INSTALL_DIR)
-
-modules-elecard-dirclean modules-elecard-clean:
-	rm -rf $(MODULES_ELECARD_PACKAGE_DIR)
-
-endif #ifeq ($(STB830_SDK),)
-
 modules-elecard: $(MODULES_ELECARD_DEPENDENCES) modules-elecard-install
 
 ifeq ($(BR2_PACKAGE_MODULES_ELECARD),y)
 TARGETS+=modules-elecard
 endif
+else #ifeq ($(STB830_SDK),)
+
+include package/elecard/sdk_env
+MODULES_ELECARD_VERSION:=$(MODULES_ELECARD_PACK_VERSION)
+MODULES_ELECARD_SOURCE:=modules-elecard-$(MODULES_ELECARD_PACK_VERSION).tar.gz
+MODULES_ELECARD_SITE:=$(ELECARD_UPLOAD_SERVER)
+
+define MODULES_ELECARD_INSTALL_TARGET_CMDS
+	TARGET_DIR=$(TARGET_DIR) make -C $(MODULES_ELECARD_DIR) -f Makefile.install
+endef
+
+$(eval $(call GENTARGETS, package/elecard, modules-elecard))
+
+# Unpack the archive. Overvrite default target, becase it uses --strip-components=1 tar option.
+$(MODULES_ELECARD_TARGET_EXTRACT):
+	@$(call MESSAGE,"Extracting")
+	mkdir -p $(MODULES_ELECARD_DIR)
+	tar -C $(MODULES_ELECARD_DIR) -xf $(DL_DIR)/$(MODULES_ELECARD_SOURCE)
+	@touch $@
+endif #ifeq ($(STB830_SDK),)
