ec13c324bcd821b857a0acc4b8f598671eac4d4c
Anton Sergeev <Anton.Sergeev@elecard.ru>
2012-11-29 12:54:15 +0700

    [+] Build: Add script for building firmware for boards with very large page nand
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

---
+++ src/firmware/bin/stb830_development_vlp_nand.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+source $(dirname $0)/default_env.sh
+
+
+# disableConfig() - comment variable in config file
+# $1 - config file
+# $2 - variable name
+#
+disableConfig() {
+	[ ! -e "$1" -o -z "$2" ] && return -1
+	sed -i "s|^$2=.*|# $2 is not set|" $1
+}
+
+# setConfig() - set variable in config file
+# $1 - config file
+# $2 - variable name
+# $3 - value, default "y"
+#
+setConfig() {
+	[ ! -e "$1" -o -z "$2" ] && return -1
+	local value=${3:-y}
+
+	if grep -E "^# $2 is not set" $1 &>/dev/null; then
+		sed -i "s|^# $2 is not set|$2=$value|" $1
+	elif grep -E "^$2=.*" $1 &>/dev/null; then
+		sed -i "s|^$2=.*|$2=$value|" $1
+	else
+		echo "$2=$value" >> $1;
+	fi
+}
+
+# addAfter() {
+# 
+# }
+
+SHORT_COMMENT=vlp_nand
+BUILDROOT_ROOTFS_CFG=$BUILDROOT/packages/buildroot/output_rootfs/.config
+
+#Reserve original config for rootfs
+TMP_ORIG_CONFIG=`mktemp /tmp/.config-XXXXXX`
+cp -f $BUILDROOT_ROOTFS_CFG $TMP_ORIG_CONFIG
+
+#Seting configs to generate jffs2 image for very large page nand
+disableConfig	$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_NANDFLASH_2K_128K
+setConfig		$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_CUSTOM
+setConfig		$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_CUSTOM_PAGESIZE		0x1000
+setConfig		$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_CUSTOM_EBSIZE		0x40000
+setConfig		$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_PAGESIZE			0x1000
+setConfig		$BUILDROOT_ROOTFS_CFG	BR2_TARGET_ROOTFS_JFFS2_EBSIZE				0x40000
+
+#Build firmware
+prjmake -C $PRJROOT firmware
+
+#Return orginal config for rootfs
+cp -f $TMP_ORIG_CONFIG $BUILDROOT_ROOTFS_CFG
+rm $TMP_ORIG_CONFIG
