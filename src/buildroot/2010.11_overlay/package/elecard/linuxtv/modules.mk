958356cc27e609deb43acc61b94a45890e3ed187
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-01-17 02:46:31 +0700

    [*] Buildroot: LinuxTV: Fix including Config.in file. Add building messages.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/linuxtv/modules.mk
+++ src/buildroot/2010.11_overlay/package/elecard/linuxtv/modules.mk
@@ -11,6 +11,8 @@ LINUXTV_DEPENDENCIES =
 LINUXTV_URL=http://smithy.elecard.net.ru/misc_repos/media_build_elc.bare
 LINUXTV_MAKE_OPTS=DIR=$(KDIR) SRCDIR=$(KDIR) ARCH=sh CROSS_COMPILE=sh4-linux- DESTDIR=$(TARGET_DIR)/ KDIR26=lib/modules/2.6.32.57_stm24_V5.0-hdk7105/linuxtv
 
+LINUXTV_MESSAGE = @echo "$(TERM_BOLD)>>> LinuxTV: $(1)$(TERM_RESET)"
+
 linuxtv-clean:
 #	make -C $(PRJROOT)/src/modules clean
 
@@ -18,26 +20,32 @@ linuxtv-dirclean:
 	rm -rf $(LINUXTV_DIR)
 
 $(LINUXTV_DIR)/.stamp_target_installed: $(LINUXTV_DIR)/.stamp_built
+	$(call LINUXTV_MESSAGE,install)
 	make -C $(LINUXTV_DIR) $(LINUXTV_MAKE_OPTS) modules_install
 	touch $@
 
 $(LINUXTV_DIR)/.stamp_built: $(LINUXTV_DIR)/.stamp_unpacked FORCE
+	$(call LINUXTV_MESSAGE,build)
 	make -C $(LINUXTV_DIR) $(LINUXTV_MAKE_OPTS)
 	touch $@
 
 $(LINUXTV_DIR)/.stamp_unpacked: $(LINUXTV_DIR)/linux/linux-media.tar.bz2
+	$(call LINUXTV_MESSAGE,unpack linux media drivers)
 	make -C $(LINUXTV_DIR) untar
 	touch $@
 
 $(LINUXTV_DIR)/.stamp_downloaded: FORCE
+	$(call LINUXTV_MESSAGE,download latest linux media drivers)
 	make -C $(LINUXTV_DIR) download
 	touch $@
 
 $(LINUXTV_DIR)/.stamp_updated: FORCE
+	$(call LINUXTV_MESSAGE,update media_build)
 	cd $(LINUXTV_DIR) && git pull
 	touch $@
 
 $(LINUXTV_DIR)/.stamp_cloned:
+	$(call LINUXTV_MESSAGE,clone media_build)
 	git clone -b SDK830 $(LINUXTV_URL) $(LINUXTV_DIR)
 	touch $@
 
