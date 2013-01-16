08608b5d72ed0356bb82ed15756c7f24fe706ee4
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-01-17 02:29:09 +0700

    [*] Buildroot: Use LinuxTV repo on smithy
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/linuxtv/modules.mk
+++ src/buildroot/2010.11_overlay/package/elecard/linuxtv/modules.mk
@@ -4,11 +4,12 @@
 #
 #############################################################
 
-LINUXTV_DIR=$(BUILDROOT)/packages/media_build
+#LINUXTV_DIR=$(BUILDROOT)/packages/media_build
+LINUXTV_DIR=$(BUILD_DIR)/media_build
 
 LINUXTV_DEPENDENCIES =
-LINUXTV_URL=/media/sda5/STM/work/linuxtv/media_build_elc.bare
-LINUXTV_MAKE_OPTS=DIR=$(KDIR) ARCH=sh CROSS_COMPILE=sh4-linux- DESTDIR=$(TARGET_DIR)/ KDIR26=lib/modules/2.6.32.57_stm24_V5.0-hdk7105/linuxtv
+LINUXTV_URL=http://smithy.elecard.net.ru/misc_repos/media_build_elc.bare
+LINUXTV_MAKE_OPTS=DIR=$(KDIR) SRCDIR=$(KDIR) ARCH=sh CROSS_COMPILE=sh4-linux- DESTDIR=$(TARGET_DIR)/ KDIR26=lib/modules/2.6.32.57_stm24_V5.0-hdk7105/linuxtv
 
 linuxtv-clean:
 #	make -C $(PRJROOT)/src/modules clean
@@ -20,11 +21,11 @@ $(LINUXTV_DIR)/.stamp_target_installed: $(LINUXTV_DIR)/.stamp_built
 	make -C $(LINUXTV_DIR) $(LINUXTV_MAKE_OPTS) modules_install
 	touch $@
 
-$(LINUXTV_DIR)/.stamp_built: $(LINUXTV_DIR)/.stamp_untar FORCE
+$(LINUXTV_DIR)/.stamp_built: $(LINUXTV_DIR)/.stamp_unpacked FORCE
 	make -C $(LINUXTV_DIR) $(LINUXTV_MAKE_OPTS)
 	touch $@
 
-$(LINUXTV_DIR)/.stamp_untar: $(LINUXTV_DIR)/linux/linux-media.tar.bz2
+$(LINUXTV_DIR)/.stamp_unpacked: $(LINUXTV_DIR)/linux/linux-media.tar.bz2
 	make -C $(LINUXTV_DIR) untar
 	touch $@
 
@@ -32,12 +33,12 @@ $(LINUXTV_DIR)/.stamp_downloaded: FORCE
 	make -C $(LINUXTV_DIR) download
 	touch $@
 
-$(LINUXTV_DIR)/.stamp_updated: 
+$(LINUXTV_DIR)/.stamp_updated: FORCE
 	cd $(LINUXTV_DIR) && git pull
 	touch $@
 
-$(LINUXTV_DIR)/.stamp_clened:
-	git clone $(LINUXTV_URL) $(LINUXTV_DIR)
+$(LINUXTV_DIR)/.stamp_cloned:
+	git clone -b SDK830 $(LINUXTV_URL) $(LINUXTV_DIR)
 	touch $@
 
 linuxtv-menuconfig:
@@ -45,15 +46,15 @@ linuxtv-menuconfig:
 
 linuxtv-install: linuxtv-build $(LINUXTV_DIR)/.stamp_target_installed
 
-linuxtv-build: linuxtv-untar $(LINUXTV_DIR)/.stamp_built
+linuxtv-build: linuxtv-unpack $(LINUXTV_DIR)/.stamp_built
 
-linuxtv-untar: linuxtv-download $(LINUXTV_DIR)/.stamp_untar
+linuxtv-unpack: linuxtv-download $(LINUXTV_DIR)/.stamp_unpacked
 
 linuxtv-download: linuxtv-update $(LINUXTV_DIR)/.stamp_downloaded
 
 linuxtv-update: linuxtv-clone $(LINUXTV_DIR)/.stamp_updated
 
-linuxtv-clone: $(LINUXTV_DIR)/.stamp_clened
+linuxtv-clone: $(LINUXTV_DIR)/.stamp_cloned
 
 linuxtv: $(LINUXTV_DEPENDENCIES) linuxtv-install
 
