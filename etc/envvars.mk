b97b2538b0263cfc2bcd7b8b292755f82f9a41e6
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-01-21 14:33:27 +0700

    [*] Build+Setup: Add using validity timestamp for setuped environment
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- etc/envvars.mk
+++ etc/envvars.mk
@@ -3,12 +3,21 @@
 ifneq "$(__ENVVARS_DEFINED__)" "y"
 export __ENVVARS_DEFINED__:=y
 
+define ECHO_SETUP_ENVIRONMENT_MESSAGE
+$(info === For SDK build:)
+$(info source <SDK_PATH>/SDK_stb830_setup.sh)
+$(info === For FULL build:)
+$(info source <SDK_PATH>/setup_stb830.sh)
+$(error )
+endef
+
 # Variable: PRJROOT
 # The root of the project tree. All makefiles must be inside this root.
 # Must be set by the environment.
 ifeq "$(origin PRJROOT)" "undefined"
 $(info === Variable PRJROOT not set: root of the project)
-$(error )
+$(info === Setup environment first.)
+$(ECHO_SETUP_ENVIRONMENT_MESSAGE)
 endif
 
 # Variable: BUILDROOT
@@ -16,22 +25,27 @@ endif
 # Must be set by the environment.
 ifeq "$(origin BUILDROOT)" "undefined"
 $(info === Variable BUILDROOT not set: directory uses for building)
-$(error )
+$(info === Setup environment first.)
+$(ECHO_SETUP_ENVIRONMENT_MESSAGE)
 endif
 
+# File: $(BUILDROOT)/timestamps/.stamp_validenvironment
+# Timestamp that identify validity curent config.
+# It can be removed from scripts, for force resetup environment.
+ifeq "$(wildcard $(BUILDROOT)/timestamps/.stamp_validenvironment)" ""
+$(info === Environment is obsolete! Please setup environment again.)
+$(ECHO_SETUP_ENVIRONMENT_MESSAGE)
+endif
 
 ifeq "$(wildcard $(BUILDROOT)/.prjconfig)" ""
 $(info === $(BUILDROOT)/.prjconfig does not exist!)
-$(info === Setup environment first:)
-$(info . ./setup_stb830.sh $(notdir $(BUILDROOT)))
-$(error )
+$(info === Setup environment first.)
+$(ECHO_SETUP_ENVIRONMENT_MESSAGE)
 endif
 
 ifneq ($(shell grep "CONFIG_VERSION" $(PRJROOT)/etc/configs/config_stb830_template), $(shell grep "CONFIG_VERSION" $(BUILDROOT)/.prjconfig))
-$(info === Configuration is updated! You should reset stb830 environment!!!)
-$(info === Please run next command in $(PRJROOT):)
-$(info . ./setup_stb830.sh $(notdir $(BUILDROOT)))
-$(error )
+$(info === Build configuration is updated! Please setup environment again!!!)
+$(ECHO_SETUP_ENVIRONMENT_MESSAGE)
 endif
 
 endif #ifneq $(__ENVVARS_DEFINED__) "y"
