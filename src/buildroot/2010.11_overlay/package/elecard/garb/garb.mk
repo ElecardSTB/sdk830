8a178098a088fdff286f3b53f2e2477c6ceab42b
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-07-05 18:40:06 +0700

    [*] Buildroot: Update garb package version.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/garb/garb.mk
+++ src/buildroot/2010.11_overlay/package/elecard/garb/garb.mk
@@ -5,7 +5,7 @@
 #############################################################
 include package/elecard/overlayTemplate.mk
 
-GARB_VERSION:=2013.07.02
+GARB_VERSION:=2013.07.05
 GARB_SOURCE:=elc-garb-bin-$(GARB_VERSION).tar.xz
 GARB_SITE:=$(ELECARD_SMITHY_TARBALLS)
 GARB_DEPENDENCIES:=libglib2_bin
