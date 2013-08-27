0a4086b2e89a04d51dda7943cde7bf2940b545b9
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-08-27 18:41:18 +0700

    [*] Rootfs+Initramfs: Add support for various behaviour of uart terminal and telnetd depending on setted envoronment.
    Add support the next build variables:
    ROOTFS_DISABLE_CONSOLE_SHELL - disables starting login or shell on uart.
    ROOTFS_DISABLE_AUTO_LOGIN - start login before enter into shell on uart.
    ROOTFS_DISABLE_TELNETD - disable starting telnet server.
    
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/initramfs/overlay/opt/elecard/bin/need_network.sh
+++ src/initramfs/overlay/opt/elecard/bin/need_network.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+export NETWORK_NEED=
+
 [% IF CONFIG_TESTSERVER_ENABLE -%]
-TEST_FIRMWARE=1
+NETWORK_NEED=1
 [% ELSE -%]
-TEST_FIRMWARE=
-[% END -%]
+
 UPNET=`/opt/elecard/bin/hwconfigManager h 0 UPNET 2>/dev/null | grep "^VALUE:" | cut -d ' ' -f 2`
 let UPNET=0x${UPNET:-0}
 UPFOUND=`/opt/elecard/bin/hwconfigManager h 0 UPFOUND 2>/dev/null | grep "^VALUE:" | cut -d ' ' -f 2`
 let UPFOUND=0x${UPFOUND:-0}
 
-export NETWORK_NEED=
-if [ "$TEST_FIRMWARE" -o $UPNET -ne 0 -o $UPFOUND -ne 0 ]; then
+if [ $UPNET -ne 0 -o $UPFOUND -ne 0 ]; then
 	NETWORK_NEED=1
 fi
+[% END -%]
