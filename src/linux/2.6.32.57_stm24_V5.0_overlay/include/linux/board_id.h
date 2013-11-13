885fcaef5fd1ecfb6be128c5f8612aff063b55f7
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-11-13 19:59:18 +0700

    [*] kernel: Add eSTB_unknown enum.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/linux/2.6.32.57_stm24_V5.0_overlay/include/linux/board_id.h
+++ src/linux/2.6.32.57_stm24_V5.0_overlay/include/linux/board_id.h
@@ -2,13 +2,12 @@
 #ifndef __BOARD_TYPE_H__
 #define __BOARD_TYPE_H__
 
-#ifdef __cplusplus
-    extern "C" {
-#endif
-
-
+/******************************************************************
+* EXPORTED TYPEDEFS                                               *
+*******************************************************************/
 typedef enum g_board_type_e {
-	eSTB830,
+	eSTB_unknown = -1,
+	eSTB830 = 0,
 	eSTB840_PromSvyaz,
 	eSTB840_PromWad,
 	eSTB840_ch7162,
@@ -18,21 +17,19 @@ typedef enum g_board_type_e {
 
 } g_board_type_t;
 
-
 #ifdef __KERNEL__
-
 struct board_special_config_s {
 	int	hdmi_hpd_inverted; //is hdmi hpd (hot plug detection) inverted, default 0
 	int	nand_wait_active_low; //is nand emi wait active low, default 1
 };
 
+/******************************************************************
+* EXPORTED FUNCTIONS PROTOTYPES               <Module>_<Word>+    *
+*******************************************************************/
 extern struct board_special_config_s *get_board_special_config(void);
 extern g_board_type_t elc_get_board_type(void);
 extern int elc_get_board_version(void);
-#endif //#ifdef __KERNEL__
 
-#ifdef __cplusplus
-}
-#endif
+#endif //#ifdef __KERNEL__
 
 #endif //#ifndef __BOARD_TYPE_H__
