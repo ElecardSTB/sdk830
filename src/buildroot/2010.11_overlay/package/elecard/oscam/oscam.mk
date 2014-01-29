b1d3826b608b4b24f8917b7c196571e9d3cc5943
Vladimir Vdovchenko <Vladimir.Vdovichenko@elecard.ru>
2014-01-29 15:14:28 +0700

    [+]Add new oscam version for master.

--- src/buildroot/2010.11_overlay/package/elecard/oscam/oscam.mk
+++ src/buildroot/2010.11_overlay/package/elecard/oscam/oscam.mk
@@ -5,7 +5,7 @@
 #############################################################
 include package/elecard/overlayTemplate.mk
 
-OSCAM_VERSION:=2013.05.21
+OSCAM_VERSION:=2014.01.20
 OSCAM_SOURCE:=elc-oscam-bin-$(OSCAM_VERSION).tar.xz
 OSCAM_SITE:=$(ELECARD_SMITHY_TARBALLS)
 OSCAM_DEPENDENCIES:=libglib2_bin
