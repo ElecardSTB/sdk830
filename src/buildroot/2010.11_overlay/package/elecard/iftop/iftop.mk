91f134fdb20105d94cb9bbae269044a4431d1832
Anton Sergeev <Anton.Sergeev@elecard.ru>
2014-06-05 17:42:50 +0700

    [*] Buildroot: Enable installing iftop into target FS
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/iftop/iftop.mk
+++ src/buildroot/2010.11_overlay/package/elecard/iftop/iftop.mk
@@ -9,10 +9,13 @@ IFTOP_SOURCE = iftop-$(IFTOP_VERSION).tar.gz
 IFTOP_SITE = http://www.ex-parrot.com/pdw/iftop/download/
 IFTOP_LIBTOOL_PATCH = NO
 IFTOP_INSTALL_STAGING = YES
-IFTOP_INSTALL_TARGET = NO
+IFTOP_INSTALL_TARGET = YES
 IFTOP_DEPENDENCIES = host-autoconf libpcap ncurses
 
 
 #IFTOP_CONF_OPT = --without-x --with-kerneldir=$(KDIR)
+define IFTOP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(IFTOP_SRCDIR)/iftop $(TARGET_DIR)/usr/sbin/iftop
+endef
 
 $(eval $(call AUTOTARGETS,package/elecard,iftop))
