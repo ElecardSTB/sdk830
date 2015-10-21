f5f80d8cb5a9218577d1353d4962425381b8a0f6
Anton Sergeev <Anton.Sergeev@elecard.ru>
2015-10-21 16:11:48 +0600

    [*] Buildroot: Update binary packets' versions
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- src/buildroot/2010.11_overlay/package/elecard/garb/garb.mk
+++ src/buildroot/2010.11_overlay/package/elecard/garb/garb.mk
@@ -7,7 +7,11 @@ include package/elecard/overlayTemplate.mk
 
 GARB_VERSION:=2014.07.28
 GARB_SOURCE:=elc-garb-bin-$(GARB_VERSION).tar.xz
+ifeq ($(STB830_SDK),)
 GARB_SITE:=$(ELECARD_SMITHY_TARBALLS)
+else
+GARB_SITE:=$(ELECARD_UPLOAD_SERVER)
+endif
 GARB_DEPENDENCIES:=libglib2_bin
 
 $(eval $(call ELC_OVERLAY_TEMPLATE,garb))
